3f384d2ab8f06e56a174f16f48a36cf4c07f8e1e
Samuel Grundman <samuel@yodas.ws>
2013-11-04 21:20:00 -0500

    start adding units
    just a soldier, temporary text-based graphics, no unit stats

---
+++ www/js/units.js
@@ -0,0 +1,10 @@
+﻿var units = [
+	{faction:'roman', type:'soldier', col:8, row:14},
+	{faction:'saxon', type:'soldier', col:10, row:10},
+	{faction:'britton', type:'soldier', col:2, row:13}
+];
+var unitTypes = {
+	soldier:{}
+//	farmer:{},
+//	worker:{}
+};
