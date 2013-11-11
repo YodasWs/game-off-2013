bb40f485ade33286f476157359cd214dd1186097
Samuel Grundman <samuel@yodas.ws>
2013-11-10 20:37:59 -0500

    Action overlays
    add overlays for legal moves on active units with keyboard shortcuts

--- www/js/units.js
+++ www/js/units.js
@@ -15,3 +15,11 @@ $.each(unitTypes, function(type, stats) {
 		action:'wait'
 	});
 });
+var actionSprites = {
+	moveU: {src:'www/img/actions/moveU.png', key:Phaser.Keyboard.U},
+	moveI: {src:'www/img/actions/moveI.png', key:Phaser.Keyboard.I},
+	moveO: {src:'www/img/actions/moveO.png', key:Phaser.Keyboard.O},
+	moveJ: {src:'www/img/actions/moveJ.png', key:Phaser.Keyboard.J},
+	moveK: {src:'www/img/actions/moveK.png', key:Phaser.Keyboard.K},
+	moveL: {src:'www/img/actions/moveL.png', key:Phaser.Keyboard.L}
+};
